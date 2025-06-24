ebfcec154828ae4279dd2151722395b627215734
StrikenowIndia <strikenowindia@gmail.com>
2025-06-24 15:55:37 +0530

    Update and rename तततत to Config.py

---
+++ Config.py
@@ -0,0 +1,12 @@
+NEWS_CATEGORIES = [
+    "Indian Politics",
+    "Global Politics",
+    "India-related Global News",
+    "Local Indian News"
+]
+
+VIDEO_LENGTH_MINUTES = 3
+LANGUAGE = "Hindi"
+UPLOAD_TIME = "daily"
+
+YOUTUBE_CREDENTIALS_FILE = "client_secrets.json"  # from Google
