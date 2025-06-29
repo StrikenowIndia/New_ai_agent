ef28f5d2c06e14894bc15a6da2635691b61ef48c
StrikenowIndia <strikenowindia@gmail.com>
2025-06-29 16:14:19 +0530

    Update and rename start.sh to s

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate virtualenv if needed (optional)
-# source venv/bin/activate
-
-# Start Flask app using gunicorn with 15-minute timeout
-gunicorn dummy_server:app --timeout 900 -b 0.0.0.0:10000
